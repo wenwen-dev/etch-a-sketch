560333968d9949583d2bf71caf938052d02f092e
Wenwen Wang <wenwen.wangwen@gmail.com>
2022-09-17 11:38:47 -0700

    Complete hover effect

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ for (let i = 0; i < 16; i++) {
   const container = document.createElement('div');
   container.classList.add('container');
   container.style.display = 'flex';
-  body.appendChild(container);
+  gridContainer.appendChild(container);
   for (let j = 0; j < 16; j++) {
     const box = document.createElement('div');
     box.classList.add('box');
     box.setAttribute('style', 'background: #B1B2FF; width: 30px; height: 30px; border: solid 1px #EEF1FF')
     container.appendChild(box);
   }
-  gridContainer.appendChild(container);
 }
 
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => box.addEventListener('mouseover', changeColor));
+
+function changeColor(e) {
+  e.target.style.background = 'lightpink';
+}
 
