f5f4b06ee3a4c0f3968cbd0f23353751aeef7d9c
Wenwen Wang <wenwen.wangwen@gmail.com>
2022-09-17 09:45:43 -0700

    Change logic from 1 container to 1 each line

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const body = document.querySelector('body');
-const container = document.createElement('div');
-container.classList.add('container');
-body.appendChild(container);
+
+
+for (let i = 0; i < 16; i++) {
+  const container = document.createElement('div');
+  container.classList.add('container');
+  container.style.display = 'flex';
+  body.appendChild(container);
+  for (let j = 0; j < 16; j++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.setAttribute('style', 'background: #B1B2FF; width: 30px; height: 30px; border: solid 1px #EEF1FF')
+    container.appendChild(box);
+  }
+}
