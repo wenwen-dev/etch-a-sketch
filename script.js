8e63ad3e74500f0d0702f6f69cbdeecacd601ee3
Wenwen Wang <wenwen.wangwen@gmail.com>
2022-09-17 11:25:30 -0700

    Question to ask why the event is not bubbling
    My understanding is that the default of bubbling is true. So when
    mouse is over a box (one single grid box) and background color
    is changed to lightpink, its container (.container)
    will also have turn into lightpink.
    
    But reality is only the single box container's background color is
    changed, not its parent.

--- script.js
+++ script.js
@@ -8,14 +8,24 @@ for (let i = 0; i < 16; i++) {
   const container = document.createElement('div');
   container.classList.add('container');
   container.style.display = 'flex';
-  body.appendChild(container);
+  gridContainer.appendChild(container);
   for (let j = 0; j < 16; j++) {
     const box = document.createElement('div');
     box.classList.add('box');
     box.setAttribute('style', 'background: #B1B2FF; width: 30px; height: 30px; border: solid 1px #EEF1FF')
     container.appendChild(box);
   }
-  gridContainer.appendChild(container);
 }
 
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => box.addEventListener('mouseover', e => {e.target.style.background = 'lightpink'; console.log(e.bubbles);}));
 
+function changeColor(e) {
+  console.log(e.bubbles);
+  console.log(e.target);
+  alert('You clicked: ' + e.target.tagName);
+  // e.stopImmediatePropagation();
+  // const test = e.target;
+  // e.target.stopPropagation;
+  e.target.style = 'background-color: red;';
+}
